3384c06ae78df9fb4e19c2b362790ad2281489f9
2428952992yz <1435759368@qq.com>
2019-11-22 08:28:19 +0800

    master

--- 21.c
+++ 21.c
@@ -2,26 +2,21 @@
 int main(void)
 {
     int a,n,sum,i,term;
-    printf("Enter a,n:");
+    printf("Enter a n:");
     scanf("%d%d",&a,&n);
     term=0;
     sum=0;
-    i=1;
-    while(i<=n){
+
+    for(i=1;i<=n;i++){
         term=term*10+a;
         sum=sum+term;
-        i++;
     }
+    
+        
+    
+        
+    
     printf("sum=%d\n",sum);
     return 0;
 }
      
-    
-    
-    
-
-
-
-
-
-    
