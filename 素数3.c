8747c66f022a2ba7fa448c1140770be54c6dc2e6
2428952992yz <1435759368@qq.com>
2019-11-29 09:33:05 +0800

    master

--- 素数3.c
+++ 素数3.c
@@ -1,34 +1,30 @@
-#include<stdio.h>
-#include<stdio.h>
-int isPrime(int m){
-    int i,n;
-    if(m==1)return 0;
-    for(i=2;i<=n/2;i++)
-     if(n%i==0){
-         return 0;
-     }
-     return 1;
+#include <stdio.h>
+#include <stdio.h>
+int isPrime(int m)
+{
+    int i;
+    if (m == 1)
+        return 0;
+    for (i = 2; i <= m / 2; i++)
+        if (m % i == 0)
+        {
+            return 0;
+        }
+    return 1;
 }
 int main(void)
 {
-    int  m,count;
-    count=0;
-    for(m=2;m<500;m++){
-        if(isPrime(m)!=0){
-            printf(" 6%d",m);
+    int m, count;
+    count = 0;
+    for (m = 2; m < 500; m++)
+    {
+        if (isPrime(m) != 0)
+        {
+            printf(" %6d", m);
             count++;
-            if(count%10==0)printf("\n");
+            if (count % 10 == 0)
+                printf("\n");
         }
-            
-    }     
+    }
     return 0;
-}  
-       
-    
-         
-
-            
-        
-    
-    
-
+}
