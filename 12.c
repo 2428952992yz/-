a31a8fb72beeb90da77f89c1757eb4e7d47b039a
2428952992yz <1435759368@qq.com>
2019-12-10 09:26:46 +0800

    master

--- 12.c
+++ 12.c
@@ -4,19 +4,26 @@ int main(void)
 {
     int i,max;
      int a[N];
-     max=a[0];
+     
     
     printf("Enter 5 numbers");
     for(i=0;i<N;i++)
     {
-        scanf("%d",&a[N]);
+        scanf("%d",&a[i]);
     }
+    max=a[0];
     for(i=1;i<N;i++)
     {
        if(a[i]>max)
+       {
          max=a[i];
+       }
+       
+     
+       
+
     } 
-        printf("%d",max);
+        printf("Max is %d\n",max);
     
        return 0;
      
