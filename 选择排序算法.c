d39a628b8b0007571d46aa82ca21101057049489
2428952992yz <1435759368@qq.com>
2019-12-13 20:15:26 +0800

    master

--- 选择排序算法.c
+++ 选择排序算法.c
@@ -1,33 +1,30 @@
 #include<stdio.h>
 int main(void)
 {
-    int i,n,b,c,d;
-    int a[10];
-    printf("Enter a n:");
-    scanf("%d",&n);
-    printf("Enter % d numbers");
-    for(i=0;i<n;i++)
+    int a[5];
+    int i,j,temp,min;
+    printf("Enter 5 numbers:");
+    for(i=0;i<5;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(b=0;b<b-1;b++)
+   
+    for(i=0;i<4;i++) 
     {
-     c=b;
-     for(i=b+1;i<n;i++)
-     {
-         if(a[i<a[c]])
-        c=i; 
-     }
-     d=a[c];
-     a[c]=a[b];
-     a[b]=c;
+        for(j=i+1;j<5;j++)
+        {
+            if(a[j]<a[i])
+               temp=a[i];
+               a[i]=a[j];
+             
+             a[j]=temp;
+           
+        } 
+    }  
+    for(i=0;i<5;i++)
+    {
+        printf("% d",a[i]);
+     
     }
-    printf("After sorted:");
-      for(i=0;i<n;i++)
-      {
-          printf("%d",a[i]);
-      }
-      printf("\n");
     return 0;
-
 }
