96232e5be35ee0273195193b243283e2e47a4d2a
2428952992yz <1435759368@qq.com>
2020-03-09 10:32:41 +0800

    master

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int main(void)
+{
+    float cm,inch;
+    printf("Enter an inch:");
+    
+    scanf("%f",&inch);
+
+    cm=inch*2.54;
+    printf("cm=%f",cm);
+    return 0;
+
+    
+}
+
+     
+    
---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int a=2,b=2,c,d,e,f;
+    
+
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=b/a;
+    printf("c=%d d=%d e=%d f=%d",c,d,e,f);
+    return 0;
+}
