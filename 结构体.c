7d27a4c1af8cc5866db58b50ec105d361076db19
2428952992yz <1435759368@qq.com>
2020-03-09 14:10:14 +0800

    master

--- 结构体.c
+++ 结构体.c
@@ -13,28 +13,26 @@ int main(void)
     struct stu_info stu[10];
     int i,n;
     float sum=0,ave;
+    scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-    scanf("%d",&n);
-    printf("please student no:\n");
-    scanf("%s",&stu[i].no);
     printf("please student name:\n");
     scanf("%s",&stu[i].name);
     scanf("%f",&stu[i].chinese);
     scanf("%f",&stu[i].math);
     scanf("%f",&stu[i].english);
-    sum=stu[i].chinese+stu[i].math+stu[i].english;
-    stu[i].ave=sum/3;
+    sum=stu[i].chinese + stu[i].math + stu[i].english;
+    stu[i].ave = sum/3.0;
     }
     int maxindex=0;
-    for(i=0;i<n;i++)
+    for(i=1;i<n;i++)
     {
         if(stu[i].ave>stu[maxindex].ave)
         {
-            maxindex=i;
+            maxindex = i;
         }
     }
-    printf("maxname=%f,maxave=%f",stu[maxindex].name,stu[maxindex].ave);
+    printf("maxname=%s,maxave=%.1f",stu[maxindex].name,stu[maxindex].ave);
     return 0;
 }
     
