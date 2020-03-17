ce4d21a4eaed1b86b1e57d5f2de491b2388e65c6
2428952992yz <1435759368@qq.com>
2020-03-17 15:29:15 +0800

    master

--- 指针进阶.c
+++ 指针进阶.c
@@ -1,7 +1,31 @@
 #include<stdio.h> 
+#include<string.h>
+#define MAXN 50
 int main(void)
 {
-   
+    char * pcity[]={"Guangzhou","Beijing","Shenzhen","Wuhan"};
+    int i,j,k;
+    char *temp;
+    for(i=0; strcmp(pcity[i],"")!=0;i++)
+    {
+        k=i;
+        for(j=i+1;strcmp(pcity[j],"")!=0;j++)
+        {
+            if(strcmp(pcity[j],pcity[k])<0)
+            k=j;
+        }
+        if(k != i)
+        {
+          temp=pcity[k];
+          pcity[k]=pcity[i];
+          pcity[i]=temp;
+        }
+    }
+     for(i=0;strcmp(pcity[i],"")!=0;i++)
+     {
+         printf("%s\n",pcity[i]);
+     }
+   return 0;
     
 }
 
