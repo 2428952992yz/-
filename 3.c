e1423916bb1e8f3d1ff6c4cc30575a465709df52
2428952992yz <1435759368@qq.com>
2020-03-21 16:11:13 +0800

    master

--- 3.c
+++ 3.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
+#include<string.h>
+char* getmonth(int n);
 int main(void)
 {
-    int a=2,b=2,c,d,e,f;
-    
-
-    c=a+b;
-    d=a-b;
-    e=a*b;
-    f=b/a;
-    printf("c=%d d=%d e=%d f=%d",c,d,e,f);
-    return 0;
+    int repeat,i,n;
+    char name;
+    scanf("%d",&repeat);
+    for(i=0;i<repeat;i++)
+    {
+    scanf("%d",&n);
+    name=getmonth(n);
+    if(n=-1){
+    printf("Wrong input!\n");
+    }
+    else printf("%s",name);
+    }
 }
+char* getmonth(int n){
+    char month[12][20]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+    if(n>0&&n<13)
+    {
+        return &month[n-1][0];
+    }
+    else return -1;
+}
+  
